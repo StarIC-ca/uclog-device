42c164a5df78166b637d71d683ab5ea17c216d24
Josh Haid <josh@staric.ca>
2026-07-21 12:26:16 -0400

    Cleanup apphash.cmake to reuse zephyr variables

--- lib/apphash.cmake
+++ lib/apphash.cmake
@@ -26,10 +26,22 @@ function(zephyr_hash_tasks)
     ${UCLOG_ROOT_DIR}/scripts/hash.py -f -i ${output}.bin -o ${output}.hash)
   set_property(GLOBAL APPEND PROPERTY extra_post_build_commands COMMAND
     ${CMAKE_OBJCOPY} --update-section .apphash=${output}.hash ${output}.elf ${output}.elf)
+
+  # variables used by zephyr/CMakeLists.txt for bin/hex generation
+  set(remove_sections_argument_list "")
+  foreach(section .comment COMMON)
+    list(APPEND remove_sections_argument_list
+      $<TARGET_PROPERTY:bintools,elfconvert_flag_section_remove>${section})
+  endforeach()
+
+  set(gap_fill_prop "$<TARGET_PROPERTY:bintools,elfconvert_flag_gapfill>")
+  set(gap_fill "$<$<BOOL:${gap_fill_prop}>:${gap_fill_prop}${CONFIG_BUILD_GAP_FILL_PATTERN}>")
+
+  # re-generate bin and hex files
   set_property(GLOBAL APPEND PROPERTY extra_post_build_commands COMMAND
-    ${CMAKE_OBJCOPY} -O binary --gap-fill 0xff --keep-section=.apphash --remove-section=.comment --remove-section=COMMON --remove-section=.eh_frame ${output}.elf ${output}.bin)
+    ${CMAKE_OBJCOPY} -O binary ${gap_fill} --keep-section=.apphash ${remove_sections_argument_list} ${output}.elf ${output}.bin)
   set_property(GLOBAL APPEND PROPERTY extra_post_build_commands COMMAND
-    ${CMAKE_OBJCOPY} -O ihex --gap-fill 0xff --keep-section=.apphash --remove-section=.comment --remove-section=COMMON --remove-section=.eh_frame ${output}.elf ${output}.hex)
+    ${CMAKE_OBJCOPY} -O ihex $<$<BOOL:${CONFIG_BUILD_OUTPUT_HEX_GAP_FILL}>:${gap_fill}> --keep-section=.apphash ${remove_sections_argument_list} ${output}.elf ${output}.hex)
 
   # Add previous post build commands back after our commands
   set_property(GLOBAL APPEND PROPERTY extra_post_build_commands ${previous_post_build_commands})
